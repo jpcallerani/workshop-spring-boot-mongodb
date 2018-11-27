6fc2cb8821703cb92e85348782620a11b915e956
Joao Silva <jpcallerani@gmail.com>
2018-11-27 20:26:07 -0200

    Projeto Criado

---
+++ src/main/java/com/br/mongo/WorkshopMongoApplication.java
@@ -0,0 +1,12 @@
+package com.br.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongoApplication.class, args);
+	}
+}
