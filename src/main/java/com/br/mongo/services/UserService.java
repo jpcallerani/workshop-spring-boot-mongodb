8041eb1c601db8121a879e2479d28bcdb04ffec3
Joao Silva <jpcallerani@gmail.com>
2018-11-28 11:40:33 -0200

    Obtendo um usuário por ID

--- src/main/java/com/br/mongo/services/UserService.java
+++ src/main/java/com/br/mongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.br.mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.br.mongo.domain.User;
 import com.br.mongo.repository.UserRepository;
+import com.br.mongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -14,8 +16,21 @@ public class UserService {
 	@Autowired
 	private UserRepository repo;
 
+	/**
+	 * 
+	 * @return
+	 */
 	public List<User> findAll() {
 		return repo.findAll();
 	}
 
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public User findById(String id ) {
+		 Optional<User> obj = repo.findById(id);  
+		 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
