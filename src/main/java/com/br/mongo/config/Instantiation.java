df8a285f445749d8cd0b3de3149319ff6139af9f
Joao Silva <jpcallerani@gmail.com>
2018-11-28 11:20:28 -0200

    Usando padrão DTO para retornar usuários

--- src/main/java/com/br/mongo/config/Instantiation.java
+++ src/main/java/com/br/mongo/config/Instantiation.java
@@ -14,17 +14,17 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		repo.deleteAll();
 
 		User joao = new User(null, "Joao Silva", "jpcallerani@gmail.com");
 		User bia = new User(null, "Bia Messias", "biahmessias@gmail.com");
 		User mari = new User(null, "Marilene Silva", "maricallerani@gmail.com");
-		
-		repo.saveAll(Arrays.asList(joao,bia,mari));
+
+		repo.saveAll(Arrays.asList(joao, bia, mari));
 
 	}
 }
