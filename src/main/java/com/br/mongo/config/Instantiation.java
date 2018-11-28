39c32f147db75120c8c60802b91c177fa8bc3e68
Joao Silva <jpcallerani@gmail.com>
2018-11-28 13:45:22 -0200

    Criando entity post com user aninhado

--- src/main/java/com/br/mongo/config/Instantiation.java
+++ src/main/java/com/br/mongo/config/Instantiation.java
@@ -1,30 +1,42 @@
 package com.br.mongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.br.mongo.domain.Post;
 import com.br.mongo.domain.User;
+import com.br.mongo.repository.PostRepository;
 import com.br.mongo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepo;
+	@Autowired
+	private PostRepository postRepo;
 
 	@Override
 	public void run(String... args) throws Exception {
 
-		repo.deleteAll();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
+
+		userRepo.deleteAll();
+		postRepo.deleteAll();
 
 		User joao = new User(null, "Joao Silva", "jpcallerani@gmail.com");
 		User bia = new User(null, "Bia Messias", "biahmessias@gmail.com");
 		User mari = new User(null, "Marilene Silva", "maricallerani@gmail.com");
 
-		repo.saveAll(Arrays.asList(joao, bia, mari));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",bia);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", bia);
+
+		userRepo.saveAll(Arrays.asList(joao, bia, mari));
+		postRepo.saveAll(Arrays.asList(post1, post2));
 
 	}
 }
---
+++ src/main/java/com/br/mongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.br.mongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.br.mongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+}
